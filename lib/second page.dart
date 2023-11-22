703d65af3062459e4d837da0adbfef85c2a57f44
Mashrur <mashrur950@gmail.com>
2023-11-22 15:15:46 +0600

    second page

---
+++ lib/second page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class second_page extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+
+}
