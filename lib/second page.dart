79d0ba5228e03eaa3b135f5f25cd33a36b8f8a51
Mashrur Rahman <mashrur950@gmail.com>
2023-11-23 03:11:32 +0600

    container

--- lib/second page.dart
+++ lib/second page.dart
@@ -5,11 +5,29 @@ class second_page extends StatefulWidget{
 }
 
 class _second_pageState extends State<second_page> {
+
   @override
   Widget build(BuildContext context) {
+    double screenW=MediaQuery.of(context).size.width;
+    double screenH=MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(),
 
-      body: Row(),
+      body: Column(
+
+        children: [
+          Container(height:(150/872)*screenH),
+          Center(
+            child: Container(
+              height:(300/872)*screenH ,
+              width: (250/392)*screenW,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+          )
+        ],
+      ),
 
     );
   }
